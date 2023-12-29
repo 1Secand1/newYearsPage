abf136614b27fa93ab1d40caf7d3619b56453fd9
1Secand1 <boevladik@gmail.com>
2023-12-29 12:34:38 +0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const red = "#ff0000";
-const blue = "#007fff";
-const silver = "#c5c9c7";
+const blue = "#001C7C";
+const purple = "#7B5BA3";
+const silver = "#BF9E7B";
 const green = "#008000";
 
-colorPassword("christmas-toy", [red, silver, blue, green], {
+colorPassword("christmas-toy", {
   christmasToy2: silver,
   christmasToy6: blue,
   christmasToy11: red,
@@ -12,7 +13,7 @@ colorPassword("christmas-toy", [red, silver, blue, green], {
   christmasToy9: red,
   christmasToy5: blue,
   christmasToy10: red,
-  christmasToy1: blue,
+  christmasToy1: purple,
   christmasToy12: green,
   christmasToy3: red,
   christmasToy7: blue,
@@ -20,34 +21,19 @@ colorPassword("christmas-toy", [red, silver, blue, green], {
 
 countdownToTheNewYear("days", "hour");
 
+textOptions();
+
+helpMessage();
+
 //_______________Utilities________________
 
-function devtoolschange(сallback) {
-  setInterval(function () {
-    let widthThreshold = window.outerWidth - window.innerWidth > 160;
-    let heightThreshold = window.outerHeight - window.innerHeight > 160;
-
-    if (widthThreshold || heightThreshold) {
-      сallback();
-    }
-  }, 1000);
-}
-
-function colorPassword(
-  classPasswordElements,
-  colorVariations,
-  passwordSettings
-) {
+function colorPassword(classPasswordElements, passwordSettings) {
+  const colorVariations = [...new Set(Object.values(passwordSettings))];
   const christmasToys = document.querySelectorAll("." + classPasswordElements);
-
   const colorPasswordIsCorrect = colorPasswordVerification(passwordSettings);
 
   const onGarland = function () {
-    // let mysterySolved = localStorage.getItem?.("mysterySolved");
-
     if (!colorPasswordIsCorrect()) return;
-
-    // localStorage.setItem("mysterySolved", " ");
 
     modalWindowSwitch();
 
@@ -56,6 +42,11 @@ function colorPassword(
     });
   };
   onGarland();
+
+  christmasToys.forEach((element) => {
+    element.style.fill =
+      colorVariations[randomValue(0, colorVariations.length - 1)];
+  });
 
   christmasToys.forEach((element) => {
     element.addEventListener(
@@ -67,25 +58,86 @@ function colorPassword(
   });
 }
 
-function countdownToTheNewYear(idElementDay, tdElementHour) {
+function colorPasswordVerification(passwordSettings) {
+  const idElementArray = Object.keys(passwordSettings);
+  const colorArray = Object.values(passwordSettings);
+
+  let colorPasswordIsCorrect = true;
+
+  const elementArray = idElementArray.map((id) => {
+    return (element = document.getElementById(id));
+  });
+
+  return () => {
+    const allColorsCorrect = elementArray.every((element, index) => {
+      const elementColor = rgbStringToHex(element.style.fill).toLowerCase();
+      const correctColor = colorArray[index].toLowerCase();
+      return elementColor === correctColor;
+    });
+
+    if (!allColorsCorrect) {
+      return false;
+    }
+    return colorPasswordIsCorrect;
+  };
+}
+
+function countdownToTheNewYear(idElementDay, idElementHour) {
   const deadline = new Date("2024", "00", "01").getTime();
 
   function setTame() {
     const currentDate = new Date().getTime();
-
-    const diff = deadline - currentDate;
-    const days = diff > 0 ? Math.floor(diff / (1000 * 60 * 60 * 24)) : 0;
+    const timeLeft = deadline - currentDate;
+    const days = Math.max(Math.floor(timeLeft / (1000 * 60 * 60 * 24)), 0);
     const hours = Math.max(
-      Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
       0
     );
 
-    let elementDay = document.getElementById(idElementDay);
-    let elementHour = document.getElementById(tdElementHour);
+    const elementDay = document.getElementById(idElementDay);
+    const elementHour = document.getElementById(idElementHour);
 
-    elementDay.innerText = days;
-    elementHour.innerText = hours;
+    if (days > 0) {
+      elementDay.innerText = `${days} ${getCorrectNounForm(days, [
+        "день",
+        "дня",
+        "дней",
+      ])} и`;
+    } else {
+      elementDay.style.display = "none";
+    }
+
+    if (hours > 0) {
+      elementHour.innerText = `${hours} ${getCorrectNounForm(hours, [
+        "час",
+        "часа",
+        "часов",
+      ])}`;
+    } else {
+      elementHour.innerText = `уже почти наступил`;
+    }
   }
+
+  function getCorrectNounForm(number, forms) {
+    if (number % 100 > 10 && number % 100 < 15) return forms[2];
+    if (number % 10 === 1) return forms[0];
+    if (number % 10 > 1 && number % 10 < 5) return forms[1];
+    return forms[2];
+  }
+
+  function getCorrectNounForm(number, forms) {
+    if (number % 100 > 10 && number % 100 < 15) {
+      return forms[2];
+    }
+    if (number % 10 === 1) {
+      return forms[0];
+    }
+    if (number % 10 > 1 && number % 10 < 5) {
+      return forms[1];
+    }
+    return forms[2];
+  }
+
   setTame();
   setInterval(setTame, 60000);
 }
@@ -104,30 +156,6 @@ function createColorSwitch(element, colors) {
   };
 }
 
-function colorPasswordVerification(passwordSettings) {
-  const idElementArray = Object.keys(passwordSettings);
-  const colorArray = Object.values(passwordSettings);
-
-  let colorPasswordIsCorrect = true;
-
-  const elementArray = idElementArray.map((id) => {
-    return (element = document.getElementById(id));
-  });
-
-  return () => {
-    for (let index = 0; index < idElementArray.length; index++) {
-      const elementСolor = rgbStringToHex(elementArray[index].style.fill);
-      const correctColor = colorArray[index];
-
-      if (elementСolor !== correctColor) {
-        return false;
-      }
-    }
-
-    return colorPasswordIsCorrect;
-  };
-}
-
 function rgbStringToHex(rgbString) {
   const regex = /rgb\((\d+), (\d+), (\d+)\)/;
   const matches = rgbString.match(regex);
@@ -139,7 +167,7 @@ function rgbStringToHex(rgbString) {
       const hex = c.toString(16);
       return hex.length == 1 ? "0" + hex : hex;
     };
-    return "#" + toHex(r) + toHex(g) + toHex(b);
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`.toLocaleLowerCase();
   } else {
     return "Invalid RGB string";
   }
@@ -147,11 +175,10 @@ function rgbStringToHex(rgbString) {
 
 function modalWindowSwitch() {
   const element = document.getElementById("modalWindow");
-  console.log(element);
   element.classList.toggle("active");
 }
 
-function devtoolsBan() {
+function devtoolsBloc() {
   let rickroll = () => {
     let hrefElement = this.document.createElement("a");
     hrefElement.setAttribute(
@@ -176,4 +203,44 @@ function devtoolsBan() {
 
   devtoolschange(rickroll);
 }
+
+function devtoolschange(сallback) {
+  setInterval(function () {
+    let widthThreshold = window.outerWidth - window.innerWidth > 160;
+    let heightThreshold = window.outerHeight - window.innerHeight > 160;
+
+    if (widthThreshold || heightThreshold) {
+      сallback();
+    }
+  }, 1000);
+}
+
+function textOptions() {
+  const deadline = new Date("2024", "00", "01").getTime();
+  const currentDate = new Date().getTime();
+
+  const timeLeft = deadline - currentDate;
+
+  if (timeLeft <= 0) {
+    document.getElementById("titleText").innerText = `Новый год уже насупил`;
+    document.getElementById("modalWindowText").innerText = ``;
+  }
+}
+
+function helpMessage() {
+  return setTimeout(() => {
+    const element = document.querySelector(".christmas-toy");
+
+    if (!element.classList.contains("active")) {
+      alert("Подсказка - внимательно присмотрись к фотографии");
+    } else {
+      console.log(5);
+    }
+  }, 100 * 10 * 60 * 4);
+}
+
+function randomValue(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // devtoolsBan();
