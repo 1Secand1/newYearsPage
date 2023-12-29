4c3ca3717147db5a8911d9db078409220ffff8d4
1Secand1 <boevladik@gmail.com>
2023-12-29 13:38:47 +0300

    .

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ const green = "#008000";
 
 colorPassword("christmas-toy", {
   christmasToy2: silver,
-  // christmasToy6: blue,
-  // christmasToy11: red,
-  // christmasToy8: red,
-  // christmasToy4: silver,
-  // christmasToy9: red,
-  // christmasToy5: blue,
-  // christmasToy10: red,
-  // christmasToy1: purple,
-  // christmasToy12: green,
-  // christmasToy3: red,
-  // christmasToy7: blue,
+  christmasToy6: blue,
+  christmasToy11: red,
+  christmasToy8: red,
+  christmasToy4: silver,
+  christmasToy9: red,
+  christmasToy5: blue,
+  christmasToy10: red,
+  christmasToy1: purple,
+  christmasToy12: green,
+  christmasToy3: red,
+  christmasToy7: blue,
 });
 
 countdownToTheNewYear("days", "hour");
